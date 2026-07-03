ad8f5ee022a1285fb746e99410c637b9ee0f2010
DeibyJG <freedeiby055@gmail.com>
2026-07-03 18:44:23 -0500

    upload V2

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,9 +3,45 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
 
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *     summary: Obtiene todos los productos disponibles en la tienda
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: Lista de productos obtenida exitosamente
+ */
 router.get('/', productController.getProducts);
 
+/**
+ * @swagger
+ * /api/products:
+ *   post:
+ *     summary: Crea un nuevo producto (Solo Admin)
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *               short_description:
+ *                 type: string
+ *               image:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Producto creado exitosamente
+ */
 router.post('/', verifyToken, verifyAdmin, productController.createProduct);
+
 router.get('/:id', productController.getProductById);
 router.put('/:id', verifyToken, verifyAdmin, productController.updateProduct);
 router.delete('/:id', verifyToken, verifyAdmin, productController.deleteProduct);
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 require('dotenv').config();
 
 const app = express();
@@ -9,8 +11,46 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Shop',
+      version: '1.0.0',
+      description: 'Documentación interactiva de la API Shop',
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT || 5000}`,
+        description: 'Servidor Local'
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Le indica a Swagger dónde leer la documentación
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+// Ruta para ver la interfaz gráfica de Swagger
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 // Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+
+})
   .then(() => console.log('Base de datos conectada'))
   .catch(err => console.log(err));
 
@@ -21,4 +61,4 @@ app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto http://localhost:${PORT}`));
